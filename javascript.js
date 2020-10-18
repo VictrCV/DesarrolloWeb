a53d030b8fe4a499b12059e0e5847fc3cfc1a03b
Víctor <victorclaramuntv@gmail.com>
2020-10-18 19:36:11 +0200

    Sustitución de Bootstrap por Materialize y añadido de la Navbar y un Carrusel. Cambio a temática de Canción de Hielo y Fuego.

---
+++ javascript.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel','.materialboxed');
+    var instances = M.Carousel.init(elems, {
+        padding: 100
+    });
+
+    var instances = M.Materialbox.init(elems, options);
+
+  });
