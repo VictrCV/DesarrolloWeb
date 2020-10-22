0249dc9c386ea3fc5ec9136c63f20e7473085b93
Víctor <victorclaramuntv@gmail.com>
2020-10-22 18:55:01 +0200

    Botón para ir arriba de la página añadido

--- javascript.js
+++ javascript.js
@@ -1,9 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel','.materialboxed');
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.carousel', '.materialboxed');
     var instances = M.Carousel.init(elems, {
         padding: 100
     });
 
     var instances = M.Materialbox.init(elems, options);
 
-  });
+});
+
+var topButtom = document.getElementById("topButton");
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topButton.style.display = "block";
+    } else {
+        topButton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
